8949c439b5f77d0112e018e1be216e6fa5af2028
NazarovMN <150329095+NazarovMN@users.noreply.github.com>
2024-10-30 09:19:41 +0200

    Update task.go

--- task/task/task.go
+++ task/task/task.go
@@ -1,10 +1,12 @@
 package task
 
 import (
+	"bufio"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -35,29 +37,90 @@ func New(taskName, taskDescription, taskStatus string, taskDue time.Time, taskPr
 	return task, nil
 }
 
-func (t *Task) UpdateName(newName string) {
-	fmt.Print("Please Change task name: ", newName)
-	fmt.Scan(&newName)
+func (t *Task) UpdateName() {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please Change task name: ")
+
+	newName, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reading input", err)
+		return
+	}
+
+	newName = strings.TrimSpace(newName)
+
 	t.Name = newName
 
+	err = t.Save()
+
+	if err != nil {
+		fmt.Println("Can't Save new info")
+		return
+	}
+
 }
 
-func (t *Task) UpdateDescription(newDescrioption string) {
-	fmt.Print("Please Change description: ", newDescrioption)
-	fmt.Scan(&newDescrioption)
+func (t *Task) UpdateDescription() {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Please Change description: ")
+
+	newDescrioption, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("Error reeading input", err)
+		return
+	}
+	newDescrioption = strings.TrimSpace(newDescrioption)
+
 	t.Description = newDescrioption
+
+	err = t.Save()
+
+	if err != nil {
+		fmt.Println("Can't Save new info")
+		return
+	}
 }
 
-func (t *Task) UpdateDueDate(date time.Time) {
-	fmt.Print("Please Change Due Date(MM-DD-YYYY): ", date)
-	fmt.Scan(&date)
+func (t *Task) UpdateDueDate() {
+	var dateStr string
+	fmt.Print("Please Change Due Date(DD-MM-YYYY): ")
+	fmt.Scan(&dateStr)
+
+	date, err := time.Parse("02-01-2006", dateStr)
+	if err != nil {
+		fmt.Println("Cant parse the time")
+		return
+	}
+
 	t.DueDate = date
+
+	err = t.Save()
+
+	if err != nil {
+		fmt.Println("Can't Save new info")
+		return
+	}
 }
 
 func (t *Task) UpdatePriority(priotirySt int) {
-	fmt.Print("Change priority status: ", priotirySt)
+	fmt.Print("Change priority status(1-High, 2-Medium, 3-Low):", priotirySt)
 	fmt.Scan(&priotirySt)
+	if priotirySt >= 3 {
+		priotirySt = 3
+	}
 	t.Priority = priotirySt
+
+	err := t.Save()
+
+	if err != nil {
+		fmt.Println("Can't Save new info")
+		return
+	}
 }
 
 func (t Task) Save() error {
@@ -79,17 +142,28 @@ func (t *Task) ShowTask() (*Task, error) {
 		fmt.Println("ty eblan failed to read from json")
 		return nil, err
 	}
-	var task *Task
+	var task Task
 
-	err = json.Unmarshal(jsonchik, task)
+	err = json.Unmarshal(jsonchik, &task)
 
 	if err != nil {
 		fmt.Println("Failed to read from json.")
 		return nil, err
 	}
 
-	return task, nil
+	return &task, nil
 
+}
+
+func (t *Task) ShowAsJson() error {
+	jsonData, err := json.MarshalIndent(t, "", " ")
+	if err != nil {
+		fmt.Println("Can't output as json format")
+		return err
+	}
+	fmt.Println(string(jsonData))
+
+	return nil
 }
 
 func (u *Task) UpdateStatus() {
@@ -109,6 +183,12 @@ func (u *Task) UpdateStatus() {
 		fmt.Println("Invalid choice.")
 	}
 
+	err := u.Save()
+
+	if err != nil {
+		fmt.Println("Can't Save new info")
+		return
+	}
 }
 
 func (t Task) OutputTaskDetails() {
